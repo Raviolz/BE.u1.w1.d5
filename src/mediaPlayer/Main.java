aeb7f2b6f26cb49b186d51c175b7f76c86c1366b
Raviolz <gi.ravioli@gmail.com>
2026-03-13 16:39:18 +0100

    LoopWithNewDynamicArray

--- src/mediaPlayer/Main.java
+++ src/mediaPlayer/Main.java
@@ -6,6 +6,7 @@ import mediaPlayer.entities.MultimediaElement;
 import mediaPlayer.entities.Video;
 import mediaPlayer.interfaces.Playable;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -21,7 +22,7 @@ public class Main {
         System.out.println(v1);
         System.out.println(i1);
 
-        // test creazione con scanner
+        // Test creazione con scanner
 
         Scanner scanner = new Scanner(System.in);
    /*     System.out.println("Inserisci il titolo del brano:");
@@ -30,7 +31,7 @@ public class Main {
         System.out.println("Inserisci volume a cui riprodurlo:");
         int volume = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("Inserisci la sua lunghezza:");
+        System.out.println("Inserisci la sua durata:");
         int duration = Integer.parseInt(scanner.nextLine());
 
         Audio a2 = new Audio(title, volume, duration);
@@ -86,7 +87,7 @@ public class Main {
         i1.show();
 
 
-        // CREAZIONE ARRAY
+        // CREAZIONE ARRAY E CICLO RICHIESTE PIU' METODO CORRISPONDENTE
 
         Video v2 = new Video("Islanda-2024", 5, 2, 5);
         Image i2 = new Image("Cactus nuovo acquisto", 7);
@@ -133,7 +134,96 @@ public class Main {
         } while (chosenNumber != 0);
 
 
+        // TEST PER CREAZIONE E IMPLEMENTAZIONE CICLO CON ARRAY DINAMICO
+
+        MultimediaElement[] dynamicMixMediaArr = new MultimediaElement[5];
+
+        // Richiesta input per creare
+
+
+        for (int i = 0; i < dynamicMixMediaArr.length; i++) {
+            System.out.println("Che tipo di elemento vuoi creare?");
+            System.out.println("1 = Audio");
+            System.out.println("2 = Video");
+            System.out.println("3 = Immagine");
+
+            // salvo per confronto switch
+
+            int choice = Integer.parseInt(scanner.nextLine());
+
+            // title comune a tutti
+
+            System.out.println("Inserisci il titolo:");
+            String title = scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Inserisci volume:");
+                    int audioVolumeInput = Integer.parseInt(scanner.nextLine());
+
+                    System.out.println("Inserisci durata:");
+                    int audioDurationInput = Integer.parseInt(scanner.nextLine());
+
+                    dynamicMixMediaArr[i] = new Audio(title, audioVolumeInput, audioDurationInput);
+                    break;
+
+                case 2:
+
+                    System.out.println("Inserisci durata:");
+                    int videoDurationInput = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Inserisci volume:");
+                    int videoVolumeInput = Integer.parseInt(scanner.nextLine());
+                    System.out.println("Inserisci luminosità:");
+                    int videoBrightnessInput = Integer.parseInt(scanner.nextLine());
+
+                    dynamicMixMediaArr[i] = new Video(title, videoDurationInput, videoVolumeInput, videoBrightnessInput);
+                    break;
+
+                case 3:
+                    System.out.println("Inserisci luminosità:");
+                    int imageBrightnessInput = Integer.parseInt(scanner.nextLine());
+
+                    dynamicMixMediaArr[i] = new Image(title, imageBrightnessInput);
+                    break;
+
+                default:
+                    System.out.println("Scelta non valida, riprova.");
+                    i--; // se no va avanti e mi stampa null cosi richiede stessa posizione
+                    break;
+            }
+        }
+
+        System.out.println(Arrays.toString(dynamicMixMediaArr));
+
+
+        // e poi stesso codice per quello hardcoded
+
+        int chosenNumberD;
+
+        do {
+            System.out.println("Scegli un file da 1 a 5, 0 per terminare: ");
+            chosenNumberD = Integer.parseInt(scanner.nextLine());
+
+            if (chosenNumberD >= 1 && chosenNumberD <= 5) {
+
+                MultimediaElement selected = dynamicMixMediaArr[chosenNumberD - 1];
+
+                // casting se no non capisce classe e non fa usare metodo
+
+                if (selected instanceof Playable) {
+                    Playable playableElementD = (Playable) selected;
+                    playableElementD.play();
+                } else {
+                    Image imageElementD = (Image) selected;
+                    imageElementD.show();
+                }
+
+            }
+
+        } while (chosenNumberD != 0);
+
     }
 
 }
+
 
